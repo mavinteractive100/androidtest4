a77b31969d6dc23d6b3a85e17d1125ab0db15a7c
ismail lawal <mavinteractive100@gmail.com>
2022-08-25 21:18:14 +0100

    second commit

--- app/src/main/java/com/example/androiddata/Global.kt
+++ app/src/main/java/com/example/androiddata/Global.kt
@@ -2,5 +2,5 @@ package com.example.androiddata
 
 const val LOG_TAG = "monsterLogging"
 
-const val WEB_SERVICE_URL = "https://ghibliapi.herokuapp.com"
+const val WEB_SERVICE_URL = "https://jsonplaceholder.typicode.com"
 const val IMAGE_BASE_URL = "$WEB_SERVICE_URL/images"
